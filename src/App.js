e6b0deb8d67ec06e9aa61f3ea011bb57880bd76b
tiago-yoneda <xyonedax@gmail.com>
2021-02-22 18:08:55 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
-
+// Tiago Yoneda
 function App() {
   return (
     <div className="App">
